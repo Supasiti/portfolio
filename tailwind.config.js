9e82c41da468797c19820f62001d5dfa6accfdac
Thara Supasiti <sapiensis@gmail.com>
2021-10-05 13:58:43 +1100

    set up prettier

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        teal: '#264653',
+        java: '#2a9d8f',
+        gold: '#e9c46a',
+        sandy: '#f4a261',
+        burnt: '#e76f51'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
