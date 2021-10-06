dbae313a09d7ce726d849b8693e987146493fa16
Thara Supasiti <sapiensis@gmail.com>
2021-10-06 17:00:23 +1100

    fix hidden project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,6 +5,7 @@ import AbsoluteDiv from '../elements/AbsoluteDiv'
 import Card from '../elements/Card'
 import Row from '../elements/Row'
 import HiddenDiv from '../elements/HiddenDiv'
+import Button from '../elements/Button'
 
 const parseImgSrcSet = (sources) => {
   const sourcesStr = sources.map((source) => {
@@ -17,32 +18,40 @@ const parseImgSrcSet = (sources) => {
   return result
 }
 
+// render
 const ProjectCard = ({ project = {} }) => {
   const [reveal, setReveal] = useState(false)
 
   return (
     <Col width="72" extend="relative p-3">
       <div
-        className="relative z-30"
+        className={`relative ${reveal ? 'z-30' : 'z-0'}`}
         onMouseEnter={() => setReveal(true)}
         onMouseLeave={() => setReveal(false)}
       >
         <a href={project.link}>
+          {/* image */}
           <img
             className="rounded-lg w-64 h-64 mx-auto object-cover object-center"
             srcSet={parseImgSrcSet(project.imgData.source)}
             alt={project.imgData.alt}
           />
-
-          <p className="px-3 pt-5">{project.title}</p>
+          <Row extend="justify-between items-center pt-3 ">
+            <span className="px-1 text-sandy">{project.title}</span>
+            <a href={project.codeLink}>
+              <Button color="gold" shadow extend="w-10 h-10 mr-1" padding="p-1">
+                <span className="fab fa-github text-2xl text-center"></span>
+              </Button>
+            </a>
+          </Row>
         </a>
       </div>
 
       <HiddenDiv isShowing={reveal}>
-        <AbsoluteDiv position="inset-x-0 top-0" extend="z-0">
-          <Card color="brand" extend="w-72 space-y-3 p-3">
-            <p className="pt-80 px-3 text-sm">{project.description}</p>
-            <Row extend="justify-center p-">
+        <AbsoluteDiv position="inset-x-0 top-0" extend="z-10">
+          <Card color="brand" extend="w-72 space-y-3 p-3 border-sandy border-2">
+            <p className="pt-80 px-1 text-sm">{project.description}</p>
+            <Row extend="justify-center">
               {project.tags.map((tag) => (
                 <span className="px-1 text-xs text-gold">{tag}</span>
               ))}
